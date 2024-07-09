63bb2adc475aea8bb894f29546229cee39b88a3c
zayd <zadfarooq624@gmail.com>
2024-07-09 13:57:12 +0530

    deployed please contribute

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import conf from "../conf/conf";
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+      maxResults: '50'
+    },
+    headers: {
+      'X-RapidAPI-Key': conf.youtubeRapidApiKey,
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+
+export const fetchFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+};
